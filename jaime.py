ae22e8fc81d826a3622cd5b39bf58e67b564616c
jaumes5 <jaumes5@hotmail.com>
2022-02-24 19:47:52 +0100

    parser

---
+++ jaime.py
@@ -0,0 +1,27 @@
+from utils import read_input
+
+
+def input_parser(file_path):
+    files = read_input(f"files/{file_path}")[0].split("\n")
+    number_of_people, number_of_projects = (int(i) for i in files.pop(0).split(" "))
+    skills_dict = {}
+    for _ in range(number_of_people):
+        person, skills = files.pop(0).split(" ")
+        for _ in range(int(skills)):
+            skill, level = files.pop(0).split(" ")
+            if skill in skills_dict:
+                skills_dict[skill][person] = int(level)
+            if skill not in skills_dict:
+                skills_dict[skill] = {person: int(level)}
+    project_dict = {}
+    for _ in range(number_of_projects):
+        project, num_days, score, best_before, number_roles = (i if pos == 0 else int(i) for pos, i in enumerate(files.pop(0).split(" ")))
+        for _ in range(int(number_roles)):
+            skill, level = files.pop(0).split(" ")
+            if project in skills_dict:
+                project_dict[project]["skills"].append((skill, int(level)))
+            if project not in skills_dict:
+                project_dict[project] = {"skills": [(skill, int(level))], "num_days": num_days, "score": score, best_before:"best_before", "number_roles": number_roles, "name": project}
+    return skills_dict, project_dict
+
+input_parser("a_an_example.in.txt")
