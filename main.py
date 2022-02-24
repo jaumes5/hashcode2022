13afcdf84ff6fa09518692984bfe9fe49e9933b6
anis <smail.a.anis@gmail.com>
2022-02-24 20:55:55 +0100

    azdzad

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils import read_input, get_score
-from anis import solve
+from anis import solve, make_output_file
 from jaime import input_parser
 
 def main():
     contributors, projects = input_parser("a_an_example.in.txt")
     assignments = solve(contributors, projects)
+    make_output_file(assignments)
     print(assignments)
 
 if __name__ == "__main__":
