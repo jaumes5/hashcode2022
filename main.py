09016925eb0b142ad9c154e6596232632dfaa700
jaumes5 <jaumes5@hotmail.com>
2022-02-24 21:21:25 +0100

    main updated

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from utils import read_input, get_score
 from anis import solve, make_output_file
 from jaime import input_parser
+import os
 
 def main():
-    contributors, projects = input_parser("a_an_example.in.txt")
-    assignments = solve(contributors, projects)
-    make_output_file(assignments)
-    print(assignments)
+    for filename in os.listdir("files"):
+        contributors, projects = input_parser(filename)
+        assignments = solve(contributors, projects)
+        make_output_file(assignments, filename)
+        print(filename, assignments)
 
 if __name__ == "__main__":
     main()
