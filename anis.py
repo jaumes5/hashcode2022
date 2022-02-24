cf5805548a0f60da5835841e4aa9d0c80f472b48
Anis <smailio@users.noreply.github.com>
2022-02-24 20:01:27 +0100

    Update anis.py

--- anis.py
+++ anis.py
@@ -1,4 +1,4 @@
-def solve(contributors, projects):
+def solve(contributors, projects_d):
     """
     projects : {"name": "Logging", "required_skills":  [{"name": "c++", "level": 2}], "days" : 3, "best_before_day" : 3, "score" : 100}
     contributors : [{"name" : "Bob", "skills" : [{"name": "c++", "level": 2}]}]
@@ -14,18 +14,18 @@ def solve(contributors, projects):
 
         for (project, start_day, team) in current_projects:
             if is_project_done(project, start_day, current_day, team):
-                done_projects.add(project)
                 busy_collaborators = busy_collaborators.difference([t["name"] for t in team])
+                del projects_d[project["name"]]
 
-        projects = sort_projects(projects, done_projects)
-
-        for (porject_name, project) in projects.items():
+        projects = sort_projects(projects_d.values())
+        for project in projects:
             if is_project_doable(project, contributors, current_day, busy_collaborators, current_projects):
                 team = make_team(project, contributors, busy_collaborators)
                 assignments += (project, team)
                 current_projects.add((project, team))
                 busy_collaborators.update([t["name"] for t in team])
 
+        
         current_day += 1    
         
     return assignments
@@ -33,7 +33,7 @@ def solve(contributors, projects):
 
 def sort_projects(projects):
     def get_project_sort_key(project):
-        return (project["best_before_day"], - project["score"] / project["days"] / len(project["required_skills"]))
+        return (project["best_before"], - project["score"] / project["num_days"] / len(project["skills"]))
     return sorted(projects, key=get_project_sort_key)
 
 def is_project_doable(project, contributors, current_day,  busy_collaborators, current_projects):
