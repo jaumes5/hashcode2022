f879f5cad8fcc208471f3ddd9811e38aa2c18bb9
anis <smail.a.anis@gmail.com>
2022-02-24 19:59:03 +0100

    integrate some code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from utils import read_input, get_score
-
+from anis import solve
+from jaime import input_parser
 
 def main():
-    pass
-
+    contributors, projects = input_parser("a_an_example.in.txt")
+    assignments = solve(contributors, projects)
+    print(assignments)
 
 if __name__ == "__main__":
     main()
