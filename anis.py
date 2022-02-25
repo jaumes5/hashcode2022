a15f14ff548c80d64d5d2c9d32f0aeb4a27a9927
jaumes5 <jaumes5@hotmail.com>
2022-02-25 11:59:08 +0100

    final optimization

--- anis.py
+++ anis.py
@@ -1,19 +1,38 @@
+import functools
+import time
+from functools import lru_cache
+
+from frozendict import frozendict
+
 from jaime import sort_collabs_skills
 
 
-def solve(contributors, projects_dict):
+contributors = {}
+projects_dict = {}
+sorted_skills = {}
+
+
+def solve(contributors_1, projects_dict_1):
     """
     projects : {"name": "Logging", "required_skills":  [{"name": "c++", "level": 2}], "days" : 3, "best_before_day" : 3, "score" : 100}
     contributors : [{"name" : "Bob", "skills" : [{"name": "c++", "level": 2}]}]
     """
+    global contributors
+    global projects_dict
+    projects_dict = projects_dict_1
+    contributors = contributors_1
     current_day = 0
 
     assignments = []
     current_projects = []
     done_projects = set()
     busy_collaborators = set()
+    treshold_time = 0.01
+    global sorted_skills
     sorted_skills = sort_collabs_skills(contributors)
+    red_list = 1
     while True:
+        start = time.time()
         if not any_project_profitable(projects_dict, current_day):
             break
 
@@ -22,44 +41,55 @@ def solve(contributors, projects_dict):
                 busy_collaborators = busy_collaborators.difference(team)
                 
 
-        projects = sort_projects(projects_dict.values())
+        projects = sort_projects(projects_dict.values(), current_day)
 
         # optimisation for later
         # if is_project_unworthy(projects[0], current_day):
         #     break
 
-        for project in projects:
+        for project in projects[:min(len(projects) // red_list + 1, len(projects))]:
             if is_project_unworthy(project, current_day):
                 del projects_dict[project["name"]]
                 continue
-            team = make_team(project, contributors, busy_collaborators)
+            team = make_team(project["name"], tuple(busy_collaborators))
             if team: 
                 assignments += [(project, team)]
                 current_projects += [(project, current_day, team)]
                 busy_collaborators.update(team)
                 del projects_dict[project["name"]]
-
         
         current_day += 1
-        
+        if current_day % 5000 == 0:
+            print(current_day, min(len(projects) // red_list + 1, len(projects)))
+        final_time = time.time() - start
+        if final_time > treshold_time:
+            red_list = int(red_list * (final_time // treshold_time))
+        # elif final_time * 100 < treshold_time and red_list >= 2:
+        #     red_list = int(red_list / 2)
+    make_team.cache_clear()
     return assignments
 
 
-def sort_projects(projects):
+def sort_projects(projects, current_day):
     def get_project_sort_key(project):
-        return (project["num_days"], project["best_before"], - project["score"] / project["num_days"] / len(project["skills"]))
-    return sorted(projects, key=get_project_sort_key)
+        tmp = 0 if current_day < project["best_before"] else current_day - project["best_before"] + 1
+        return (project["score"] - tmp) / project["num_days"]
+    return sorted(projects, key=get_project_sort_key, reverse=True)
 
 def is_project_doable(project, contributors, current_day,  busy_collaborators, current_projects):
     pass
 
 
-def make_team(project, contributors, busy_collaborators, sorted_skills):
+@lru_cache
+def make_team(project_name, busy_collaborators):
+    project = projects_dict[project_name]
     skills = project['skills']
-    cont = set(list(contributors.values())[0].keys()) - busy_collaborators
+    cont = set(list(contributors.values())[0].keys()) - set(busy_collaborators)
     team = []
     if len(cont) >= len(skills):
         for skill, value in skills:
+            if value > max(sorted_skills[skill], key=lambda a: a[1])[1]:
+                return 0
             no_better = False
             for collab, _ in sorted_skills[skill]:
                 if contributors[skill][collab] >= value and collab not in team and collab in cont:
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from utils import read_input, get_score
 from anis import solve, make_output_file
 from jaime import input_parser
 import os
+import time
 
 def main():
+    start = time.time()
     for filename in os.listdir("files"):
         contributors, projects = input_parser(filename)
         assignments = solve(contributors, projects)
         make_output_file(assignments, filename)
         print(filename, assignments)
+    final_time = time.time() - start
+    print(f"Time elpased {final_time // 60} min and {int(final_time % 60)} sec")
 
 if __name__ == "__main__":
     main()
